1ffaed968a7869679f67f83d06c87fda38e5d7ec
Alinur Mirassov <a.mirassov@kazdream.kz>
2022-08-01 18:49:19 +0600

    another one

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {ProductComponent} from "./components/product/product.component";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import {ProductComponent} from "./components/product/product.component";
     ProductComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
